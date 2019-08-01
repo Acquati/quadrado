4efbd0748afe7a8cdf9bfca50695e54a3b3c1bcb
Leandro Acquati <leandroacquati@gmail.com>
2019-08-01 02:24:50 -0300

    complete grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,9 @@
 <template>
   <div v-if="is_data_fetched" class="container">
-    <div style="display: inline-block;">
-      {{ $vssWidth }}
-    </div>
-    <div style="display: inline-block;">
-      {{ $vssHeight }}
-    </div>
-    <div />
-    <div
-      v-for="(i, index) in $vssWidth"
-      :key="`gridRow-${index}`"
-      class="gridRow"
-    />
+    <!-- <div class="info">
+      <div>{{ $vssWidth }} x {{ $vssHeight }}</div>
+    </div> -->
+    <div v-for="x in windowX" :key="`gridX-${x}`" class="gridBlock" />
   </div>
 </template>
 
@@ -20,6 +12,14 @@ export default {
   data() {
     return {
       is_data_fetched: false
+    }
+  },
+  computed: {
+    windowX() {
+      return Math.floor(this.$vssWidth / 30) * Math.floor(this.$vssHeight / 30)
+    },
+    windowY() {
+      return Math.floor(this.$vssHeight / 30)
     }
   },
   mounted() {
@@ -32,12 +32,28 @@ export default {
 .container {
   margin: 0 auto;
   min-height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
 }
 
-.gridRow {
-  background-color: black;
-  height: 1px;
-  width: 1px;
-  display: inline-block;
+.gridBlock {
+  background-color: white;
+  border: 1px solid gray;
+  box-sizing: border-box;
+  height: 30px;
+  width: 30px;
+}
+
+.info {
+  position: relative;
+
+  div {
+    position: absolute;
+    display: inline;
+    color: red;
+  }
 }
 </style>
